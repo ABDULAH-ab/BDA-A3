2ea916527ab20e38392516caf40bb04094665742
Shazer-Nadeem <165384862+Shazer-Nadeem@users.noreply.github.com>
2024-04-24 19:45:33 +0500

    pre_process.py

--- pre_process.py
+++ pre_process.py
@@ -1,12 +1,5 @@
 import json
 import os
-from pymongo import MongoClient
-
-# MongoDB configuration
-# MONGO_HOST = 'localhost'
-# MONGO_PORT = 27017
-# DATABASE_NAME = 'amazon_data'
-# COLLECTION_NAME = 'filtered_data'
 
 def batch_process_file(input_path, output_path, batch_size=10000):
     with open(input_path, 'r') as input_file:
@@ -16,29 +9,19 @@ def batch_process_file(input_path, output_path, batch_size=10000):
             batch.append(json.loads(line))
             if len(batch) >= batch_size:
                 filtered_data = filter_data(batch)
-                # output(output_path, filtered_data)
+                output(output_path, filtered_data)
                 batch.clear() 
         if batch:
             filtered_data = filter_data(batch)
-            # output(output_path, filtered_data)
-            batch.clear()
-            
-    # MongoDB connection
-    # client = MongoClient(MONGO_HOST, MONGO_PORT)
-    # db = client[DATABASE_NAME]
-    # collection = db[COLLECTION_NAME]
-    # for data_item in filtered_data:
-    #     collection.insert_one(data_item)
-    # client.close()
+            output(output_path, filtered_data)
+            batch.clear() 
 
 
 def filter_data(data):
     filtered_data = []
     for item in data:
         filtered_item = {
-            "asin": item.get("asin", None),
-            "also_buy": item.get("also_buy", None),
-            "also_view": item.get("also_view", None)
+            "also_buy": item.get("also_buy", None)
         }
         filtered_data.append(filtered_item)
     return filtered_data
